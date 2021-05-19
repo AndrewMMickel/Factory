22f43e168919a4466e44bcc0c64d9d63a2024c10
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-05-19 13:00:11 -0700

    create models and implement code

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+    public class Engineer
+    {
+        public Engineer()
+        {
+            this.Machines = new HashSet<EngineerMachine>();
+
+        }
+        public int EngineerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return this.LastName + ", " + this.FirstName; } }
+        public ICollection<EngineerMachine> Machines { get; }
+    }
+}
